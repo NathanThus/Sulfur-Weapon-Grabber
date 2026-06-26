b98eb46fbe04f194f32f6f86de8b775538c09d4d
verdictfps <verdictfps@gmail.com>
2026-06-26 18:45:14 -0400

    Enhance weapon data serialization by adding recoil properties and base caliber conversion

--- Plugin.cs
+++ Plugin.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using BepInEx;
 using BepInEx.Logging;
 using Newtonsoft.Json;
 using PerfectRandom.Sulfur.Core;
 using PerfectRandom.Sulfur.Core.CharacterStats;
 using PerfectRandom.Sulfur.Core.Items;
+using PerfectRandom.Sulfur.Core.Weapons;
 using UnityEngine;
 
 namespace WeaponDataGrabber;
@@ -19,6 +21,18 @@ public class Plugin : BaseUnityPlugin
     CaliberType[] Caliberdatabase;
     List<ItemDefinition> weaponDatabase;
     DatabaseGrabber grabber = new();
+
+    float RecoilHelper(List<CaliberKickDefinition> kickPowerList, string caliber)
+    {
+        foreach (var kick in kickPowerList)
+        {
+            if (kick.Caliber.ToString() == caliber)
+            {
+                return kick.KickPower;
+            }
+        }
+        return 0;
+    }
     private IEnumerator Start()
     {
         while (!StaticInstance<AsyncAssetLoading>.Instance.loadingDone)
@@ -43,9 +57,19 @@ public class Plugin : BaseUnityPlugin
                 weaponList.Add(new WeaponDTO
                 {
                     name = weaponSO.displayName,
+                    baseCaliber = EnumConversion.CaliberTypeToString(weaponSO.caliber),
                     caliberDamage = Caliberdatabase[(int)weaponSO.caliber].baseDamage,
                     innateDamageMultiplier = weaponSO.damageMultiplier,
-                    weaponTypeMultiplier = WeaponTypeDataExt.GetDamageMultiplier(weaponSO.weaponType)
+                    weaponTypeMultiplier = WeaponTypeDataExt.GetDamageMultiplier(weaponSO.weaponType),
+                    calculatedWeaponDamage = (weaponSO.damageMultiplier * WeaponTypeDataExt.GetDamageMultiplier(weaponSO.weaponType)) * Caliberdatabase[(int)weaponSO.caliber].baseDamage,
+                    numberOfProjectiles = Caliberdatabase[(int)weaponSO.caliber].numberOfProjectiles,
+                    roundsPerMinute = weaponSO.rpm,
+                    spread = weaponSO.Spread,
+                    recoil9mm = RecoilHelper(weaponSO.kickPower, "_9mm"),
+                    recoil556 = RecoilHelper(weaponSO.kickPower, "_556mm"),
+                    recoil762 = RecoilHelper(weaponSO.kickPower, "_762mm"),
+                    recoil50bmg = RecoilHelper(weaponSO.kickPower, "_50BMG"),
+                    recoil12ga = RecoilHelper(weaponSO.kickPower, "_12ga"),
                 });
             }
         }
