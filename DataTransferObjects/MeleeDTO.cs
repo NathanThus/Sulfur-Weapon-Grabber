0632536829bc23dad0b7b163b18fc03399d4c148
NathanThus <nathan.thus@outlook.com>
2026-06-30 17:02:00 +0200

    Add Constructor methods to DTO objects

--- DataTransferObjects/MeleeDTO.cs
+++ DataTransferObjects/MeleeDTO.cs
@@ -1,6 +1,16 @@
 using System;
+using PerfectRandom.Sulfur.Core.Items;
 
 [Serializable]
 public class MeleeDTO : BaseDTO
 {
+    public static MeleeDTO CreateMeleeDTO(WeaponSO weaponSO)
+    {
+        return new MeleeDTO
+        {
+            name = weaponSO.name,
+            displayName = weaponSO.displayName,
+            weaponType = EnumConversion.WeaponClassToString(weaponSO.weaponType)
+        };
+    }
 }
--- DataTransferObjects/WeaponDTO.cs
+++ DataTransferObjects/WeaponDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using PerfectRandom.Sulfur.Core;
 using PerfectRandom.Sulfur.Core.CharacterStats;
 using PerfectRandom.Sulfur.Core.Items;
 using PerfectRandom.Sulfur.Core.Stats;
@@ -28,4 +29,34 @@ public class WeaponDTO : BaseDTO
     public float timeToCooldownSpread;
     public string damageType;
     public string projectileType;
+
+    public static WeaponDTO CreateWeaponDTO(WeaponSO weaponSO, ValueHelpers helpers)
+    {
+        var caliberEntry = DatabaseGrabber.GetCaliberEntry(weaponSO);
+
+        return new WeaponDTO
+        {
+            name = weaponSO.name,
+            displayName = weaponSO.displayName,
+            baseCaliber = EnumConversion.CaliberTypeToString(weaponSO.caliber),
+            baseCaliberDmgPerProj = caliberEntry.baseDamage,
+            innateDamageMultiplier = weaponSO.damageMultiplier,
+            weaponTypeMultiplier = WeaponTypeDataExt.GetDamageMultiplier(weaponSO.weaponType),
+            calculatedWeapDmgPerProj = helpers.CalculatedBaseWeaponDamage(weaponSO, caliberEntry.baseDamage),
+            numberOfProjectiles = caliberEntry.numberOfProjectiles,
+            roundsPerMinute = weaponSO.rpm,
+            spread = helpers.GetCaliberSpread(weaponSO.spreadPerCaliber),
+            recoil = helpers.GetCaliberRecoil(weaponSO.kickPower),
+            magazineSize = weaponSO.iAmmoMax,
+            ammoPerShot = weaponSO.iMaxAmmoPerShot,
+            bulletSpeed = weaponSO.bulletSpeed,
+            weightClass = weaponSO.weightClass,
+            //weaponWeight = protectedHelpers.ExposeWeightClassConversion(weaponSO.
+            shotsToReachFullSpread = weaponSO.shotsToReachFullSpread,
+            timeToCooldownSpread = weaponSO.timeToCooldownSpread,
+            damageType = EnumConversion.DamageTypeToString(weaponSO.damageType),
+            projectileType = EnumConversion.ProjectileTypeToString(weaponSO.projectileType),
+            weaponType = EnumConversion.WeaponClassToString(weaponSO.weaponType),
+        };
+    }
 }
